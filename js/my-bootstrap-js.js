ea4a7eff30a8bf42caa5ee7c9c554220a08a2da1
beatface <emma.beaton@gmail.com>
2016-02-07 14:48:12 -0600

    added photos, videos to music page

---
+++ js/my-bootstrap-js.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	$('a[rel=popover]').popover({
+		html: true,
+		trigger: 'hover',
+		placement: 'right',
+		content: function(){return '<img src="'+$(this).data('img') + '" />';}
+	});
+
+
+
+
+});
