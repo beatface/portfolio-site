439e78fd145a40a3498bc23ee87e89362eb597b9
beatface <emma.beaton@gmail.com>
2016-02-09 14:26:55 -0600

    converted everything to jade and completed refactor into express

--- app.js
+++ app.js
@@ -3,3 +3,31 @@
 const express = require('express');
 const path = require('path');
 const _ = require('lodash');
+
+
+
+const app = express();
+const { PORT } = process.env;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+	res.render('index');
+});
+
+app.get('/music', function(req, res) {
+	res.render('music');
+});
+
+app.get('/dev', function(req, res) {
+	res.render('dev');
+});
+
+
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port 3000!');
+});
